30f710e9ae8da31a935b61a88036b79f5ac77ef6
Tunoc <random959dude@gmail.com>
2020-10-28 12:21:05 +0100

    Period2 23-10-2020 test fix

--- Period2/(23-10-2020) - Testing JavaScript Backend Code With Mocha And Chai/readyForDay3/test/userFacadeTest.ts
+++ Period2/(23-10-2020) - Testing JavaScript Backend Code With Mocha And Chai/readyForDay3/test/userFacadeTest.ts
@@ -1,9 +1,11 @@
 import UserFacade from '../src/facades/userFacade';
-import { expect, should } from "chai";
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
+const expect = chai.expect
+const assert = chai.assert
 import { ApiError } from '../src/errors/apiError';
 import { bryptAsync, bryptCheckAsync } from '../src/utils/bcrypt-async-helper'
-import IGameUser from '../src/interfaces/GameUser'
-import { assert, debug } from 'console';
 
 describe("Verify the UserFacade", function () {
   beforeEach(async function () {
@@ -23,46 +25,33 @@ describe("Verify the UserFacade", function () {
       const passwordOK = await bryptCheckAsync("secret", jan.password);//Fails if the password isn't "secret"
       expect(status).to.be.equal("User was added")
       expect(UserFacade.users.length).to.equal(4)
+      expect(passwordOK).to.be.equal(true);
     } catch (err) {
       expect.fail("Seems like password was not hashed correctly")
     } finally { }
   })
 
   it("Should remove the user Peter", async function () {
-    try {
-      const status = await UserFacade.deleteUser("pp@b.dk");
-      expect(status).to.be.equal("User was deleted")
-    } catch (err) {
-      expect.fail("The user wasn't deleted")
-    } finally { }
+    const status = await UserFacade.deleteUser("pp@b.dk");
+    expect(status).to.be.equal("User was deleted")
   })
 
   it("Should get three users", async function () {
-    try {
-      const status = await UserFacade.getAllUsers();
-      expect(status.length).to.be.equal(3);
-      expect(status[0].name).to.be.equal("Peter Pan")
-    } catch (err) {
-      expect.fail("3 users not found")
-    } finally { }
+    const status = await UserFacade.getAllUsers();
+    expect(status.length).to.be.equal(3);
+    expect(status[0].name).to.be.equal("Peter Pan")
+    expect(status[1].name).to.be.equal("Donald Duck")
+    expect(status[2].name).to.be.equal("admin")
   })
 
   it("Should find Donald Duck", async function () {
-    try {
-      const status = await UserFacade.getUser("dd@b.dk");
-      expect(status.name).to.be.equal("Donald Duck")
-    } catch (err) {
-      expect.fail("Couldn't find Donald Duck")
-    } finally { }
+    const findDonald = await UserFacade.getUser("dd@b.dk");
+    expect(findDonald.name).to.be.equal("Donald Duck");
   })
 
   it("Should not find xxx.@.b.dk", async function () {
-    try {
-      await UserFacade.getUser("xxx.@.b.dk");
-      //Expecting to fail
-    } catch (err) {
-      expect(err).to.be.instanceOf(Error);
-      expect(err.message).to.equal("User Not Found");
-    } finally { }
+    let throwsError = UserFacade.getUser("xxx.@.b.dk")
+    //Needs to await the expect, or else the test will still show passing even though it failed.
+    await expect(throwsError).to.be.rejectedWith(ApiError, "User Not Found");
   })
 })
